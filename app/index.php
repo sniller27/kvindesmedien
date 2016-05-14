668cec03c560903f031e57b427fea03297ba3d63
Jonas <sniller27@hotmail.com>
2016-05-14 14:51:14 +0200

    frontpage skeleton and hover effects

--- app/index.php
+++ app/index.php
@@ -7,32 +7,75 @@
 	<?php include 'php/head.php'; ?>
 
 </head>
-<body>
+<body class="frontpagecover">
 
 	<div id="wrapper">
 
-	<!-- coverimage -->
-	<div class="coverimage">
-	
 		<!-- header -->
 		<?php include 'php/header.php'; ?>
 
-	</div>
+		
+			<div class="griddo griddo-pad backgroundlayer">
+				<div class="frontpagecontainer">
+			      <div class="col-1-1">
+			      	<div class="frontpagesemicover">
+			      		<div class="imagetextheader">
+			      			<h2 style="margin:0px; color:white;">Velkommen til Kvindesmedien på Christiania</h2>
+			      			<p style="margin:0px; color:white;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			      		</div>
+			      	</div>
+			      </div>
+			      <div class="col-1-3 .mobile-col-1-2">
+			      <a href="#">
+			      	<div class="frontpagebox1">
+			      		<div class="imagetextsmall">
+			      			<h2 style="margin:0px; color:white;">Se vores mange skulpturer</h2>
+			      		</div>
+			      	</div>
+			      </a>
+			      </div>
+			      <div class="col-1-3 .mobile-col-1-2">
+			      	<a href="#">
+				      	<div class="frontpagebox2">
+				      		<div class="imagetextsmall">
+				      			<h2 style="margin:0px; color:white;">Se vores mange skulpturer</h2>
+				      		</div>
+				      	</div>
+			      	</a>
+			      </div>
+			      <div class="col-1-3 .mobile-col-1-2">
+			      	<a href="#">
+				      	<div class="frontpagebox3">
+				      		<div class="imagetextsmall">
+				      			<h2 style="margin:0px; color:white;">Se vores mange skulpturer</h2>
+				      		</div>
+				      	</div>
+			      	</a>
+			      </div>
+				    <div class="col-1-2 .mobile-col-1-1" style="color: white;">
+				    <h2>Hvem er vi?</h2>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				    <a href="#">Læse mere her...</a>
+				    </div>
+					<div class="col-1-2">
+				      <img src="images/ejere.jpg" style="width: 100%">
+				    </div>
+				    <div class="col-1-1 newsletterfrontpage">
+					    <form>
+					    	<h2>Tilmeld dig vores nyhedsbrev</h2>
+					    	<input type="text"></input>
+					    	<button class="btn">Tilmeld</button>
+					    </form>
+				    </div>
 
-	
-<img src="images/slider.jpg">
-	<div class="griddo griddo-pad">
-      <div class="col-1-1">
-        <div class="smallcollage">
-		hej
+			    </div>
+			</div>
+		
+
+		<!-- Footer -->
+		<div id="footer"></div>
+
 		</div>
-      </div>
-	</div>
-
-	<!-- Footer -->
-	<div id="footer"></div>
-
-	</div>
 
 </body>
 </html>
